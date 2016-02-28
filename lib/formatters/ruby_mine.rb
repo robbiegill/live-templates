35d98690694ef170cd56543480ba25b9daa9e4ab
Robbie Gill <robbie@zenpayroll.com>
2016-02-27 21:34:56 -0800

    react + rubymine formatter

---
+++ lib/formatters/ruby_mine.rb
@@ -0,0 +1,80 @@
+require 'yaml'
+require 'nokogiri'
+
+module Formatters
+  class RubyMine
+    def self.dump!(file)
+      group = LiveTemplateGroup.new(file)
+
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.templateSet(group: group.name) do
+          group.templates.each do |template|
+            xml.template(template.attributes) do
+              template.variables do |var|
+                xml.variable(name: var, expression: '', defaultValue: '', alwaysStopAt: 'true')
+              end
+              xml.context_ do
+                group.contexts.each { |name| xml.option(name: name, value: 'true') }
+              end
+            end
+          end
+        end
+      end
+
+      puts builder.to_xml
+    end
+
+    class LiveTemplateGroup
+      attr_reader :data
+
+      def initialize(file_name)
+        @data = YAML.load_file File.join('templates', "#{file_name}.yml")
+      end
+
+      def name
+        data['name']
+      end
+
+      def templates
+        data['templates'].map { |name, data| Template.new name, data }
+      end
+
+      def contexts
+        data['contexts']
+      end
+
+      class Template
+        attr_reader :name, :data
+
+        def initialize(name, data)
+          @name = name
+          @data = data
+        end
+
+        def attributes
+          {
+            name: name,
+            value: value,
+            description: description,
+            toReformat: 'true',
+            toShortenFQNames: 'true'
+          }
+        end
+
+        def variables
+          value.scan(/\$\S*\$/).map{ |m| m.tr '$', '' }.each do |var|
+            yield var unless var == 'END'
+          end
+        end
+
+        def description
+          data['description']
+        end
+
+        def value
+          data['template']
+        end
+      end
+    end
+  end
+end
